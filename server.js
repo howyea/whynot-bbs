0eb3f50517695aeec3cf568cf3a718407e0f77a4
huiyuye <yhynew@126.com>
2019-08-07 11:53:57 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const nodemon = require('nodemon');
+const path = require('path');
+
+nodemon({
+  execMap: {
+    js: 'node'
+  },
+  script: path.join(__dirname, 'app'),
+  ignore: [],
+  watch: process.env.NODE_ENV !== 'production' ? ['*'] : false,
+  ext: 'js'
+})
+.on('restart', function() {
+  console.log('Server restarted!');
+})
+/* .once('exit', function () {
+  console.log('Shutting down server');
+  process.exit();
+}) */;
